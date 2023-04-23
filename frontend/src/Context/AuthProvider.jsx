f8d54008e6639bf96f8c09cd3e32875c1fa4e2cd
Durjoy-dcp <unknownccxv@gmail.com>
2023-04-24 01:00:45 +0600

    authProvider

--- frontend/src/Context/AuthProvider.jsx
+++ frontend/src/Context/AuthProvider.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createContext } from "react";
+import app from "../Firebase/firebase-init";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
 export const AuthContext = createContext();
-
+const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
-  const authInfo = {};
+  const [user, setuser] = useState("");
+  const [loading, seLoading] = useState(true);
+  const signup = (email, password) => {
+    seLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const login = (email, password) => {
+    seLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const logOut = () => {
+    return signOut(auth);
+  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setuser(currentUser);
+      seLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+  const authInfo = { user, loading, seLoading, signup, login, logOut };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
